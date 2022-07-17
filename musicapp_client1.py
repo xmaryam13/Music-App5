4bfcc6388a329436c883f5c9785446c8e741c235
xmaryam13 <90093129+xmaryam13@users.noreply.github.com>
2022-07-17 15:36:29 +0100

    Add files via upload

---
+++ musicapp_client1.py
@@ -0,0 +1,183 @@
+from importlib.metadata import files
+import socket
+from threading import Thread
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from playsound import playsound
+import pygame
+from pygame import mixer 
+import os
+import time
+import ftplib
+from ftplib import FTP
+import ntpath 
+from pathlib import Path 
+
+
+
+PORT = 8050
+IP_ADDRESS = '127.0.0.1'
+SERVER = None
+BUFFER_SIZE = 4096
+
+
+name = None
+listbox = None
+filePathLabel = None
+
+global song_counter
+song_counter = 0
+
+def browseFiles():
+    global listbox 
+    global song_counter
+    global filePathLabel
+    
+    try:
+        filename = filedialog.askopenfilename()
+        HOSTNAME = '127.0.0.1'
+        USERNAME = 'lftpd'
+        PASSWORD = 'lftpd'
+        
+        ftp_server = FTP(HOSTNAME, USERNAME, PASSWORD)
+        ftp_server.encoding = 'utf-8'
+        ftp_server.cwd('shared_files')
+        fname = ntpath.basename(filename)
+        with open(filename, 'rb') as file:
+            ftp_server.storbinary(f"STOR {fname}", file)
+        
+        ftp_server.dir()
+        ftp_server.quit()
+        
+        listbox.insert(song_counter, fname)
+        song_counter = song_counter + 1
+        
+    
+    except FileNotFoundError:
+        print('Cancel Button Pressed')
+        
+
+for file in os.listdir('shared_files'):
+    filename = os.fsdecode(file)
+    listbox.insert(song_counter,filename)
+    song_counter = song_counter + 1
+    
+    
+def download():
+    song_to_download = listbox.get(ANCHOR)
+    infoLabel.configure(text = 'Downloading'+ song_to_download)
+    HOSTNAME = '127.0.0.1'
+    USERNAME = 'lftpd'
+    PASSWORD = 'lftpd'
+    home = str(Path.home())
+    download_path = home+'/Downloads'
+    ftp_server = ftplib.FTP(HOSTNAME,USERNAME,PASSWORD)
+    ftp_server.encoding = 'utf-8'
+    ftp_server.cwd('shared_files')
+    local_filename = os.path.join(download_path,song_to_download)
+    file = open(local_filename,'wb')
+    ftp_server.retrbinary('RETR '+ song_to_download,file.write)
+    ftp_server.dir()
+    file.close()
+    ftp_server.quit()
+    infoLabel.configure(text='Download Complete')
+    time.sleep(1)
+    if (song_counter != ""):
+        infoLabel.configure(text='Now Playing' +song_selected)
+    else:
+        infoLabel.configure(text="")
+    
+    
+    
+    
+def play():
+    global song_selected
+    song_selected = listbox.get(ANCHOR)
+    
+    pygame
+    mixer.init()
+    mixer.music.load('shared_files/' + song_selected)
+    mixer.music.play()
+    if (song_selected != ""):
+        infoLabel.configure(text='Now Playing: ' + song_selected)
+    else:
+        infoLabel.configure(text="")
+        
+
+def stop():
+    global song_selected
+    pygame
+    mixer.init()
+    mixer.music.load('shared_files/', song_selected)
+    mixer.music.pause()
+    infoLabel.configure(text= "")
+    
+def resume():
+    global song_selected
+    
+    mixer.init()
+    mixer.music.load('shared_files'+song_selected)
+    mixer.music.play()
+    
+def pause():
+    global song_selected
+    pygame
+    mixer.init()
+    mixer.music.load('shared_files'+song_selected)
+    mixer.music.pause()
+
+
+
+def musicWindow():
+    window = Tk()
+    window.title('Music Window')
+    window.geometry('300x300')
+    window.configure(bg = 'LightSkyBlue')
+    
+    selectLabel = Label(window, text = 'Select Song', bg = 'LightSkyBlue', font = ('Calibri',10))
+    selectLabel.place(x=2,y=2)
+    
+    listBox = Listbox(window, height = 10, width = 39, activestyle = 'dotbox',bg = 'LightSkyBlue', borderwidth = 2, font = ('Calibri',10))
+    listBox.place(x=12,y=20)
+    
+    scrollbar1 = Scrollbar(listBox)
+    scrollbar1.place(relheight = 1, relx=1)
+    scrollbar1.config(command = listBox.yview)
+    
+    playButton = Button(window, text='Play',width = 10, bd = 1, bg = 'SkyBlue',font = ('Calibri',10),command = play)
+    playButton.place(x=30,y=200)
+    
+    stopButton = Button(window, text='Stop',bd=1,width=10,bg='SkyBlue',font = ('Calibri',10),command = stop)
+    stopButton.place(x=200,y=200)
+    
+    uploadButton = Button(window, text = 'Upload',width = 10, bd = 1, bg = 'SkyBlue',font = ('Calibri',10))
+    uploadButton.place(x = 30,y=250)
+    
+    downloadButton = Button(window, text='Download',width = 10,bd=1,bg='SkyBlue',font=('Calibri',10))
+    downloadButton.place(x=200,y=250)
+    
+    infoLabel = Label(window, text = '',fg = 'blue',font = ('Calibri',10))
+    infoLabel.place(x=4,y=200)
+    
+    ResumeButton = Button(window,text='Resume',width = 10,bd=1,bg='SkyBlue',font=('Calibri',10),command=resume)
+    ResumeButton.place(x=50,y=250)
+    
+    PauseButton = Button(window,text='Pause Button',bd=1,bg='SkyBlue',font=('Calibri',10),command=pause)
+    PauseButton.place(x=230,y=250)    
+    
+    
+    window.mainloop()
+    
+
+def set_up():
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+    
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.connect((IP_ADDRESS,PORT))
+    
+    musicWindow()
+    
+set_up()
---
+++ musicapp_server1.py
@@ -0,0 +1,81 @@
+from distutils.core import setup
+import socket 
+from threading import Thread
+import os
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+IP_ADDRESS = '127.0.0.1'
+PORT = 8050
+SERVER = None 
+BUFFER_SIZE = 4096
+clients = {}
+
+
+def ftp():
+    global IP_ADDRESS
+    authorizer = DummyAuthorizer()
+    authorizer.add_user('lftpd','lftpd','.',perm = 'elradfmw')
+    
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    
+    ftp_server = FTPServer((IP_ADDRESS,21),handler)
+    ftp_server.serve_forever()
+    
+setup_thread = Thread(target = setup)
+setup_thread.start()
+
+ftp_thread = Thread(target = ftp)
+ftp_thread.start()
+
+
+is_dir_exists = os.path.isdir('shared_files')
+print(is_dir_exists)
+if (not is_dir_exists):
+    os.makedirs('shared_files')
+
+
+
+def acceptConnection():
+    global SERVER
+    global clients
+    
+    while True:
+        client, addr = SERVER.accept()
+        client_name = client.recv(4096).decode().lower()
+        clients[client_name] = {
+            'client' : client,
+            'address' : addr,
+            'connected_with' : '',
+            'file_name' : '',
+            'file_size' : 4096}
+        
+        print(f"Connection established with {client_name} : {addr}")
+        
+        thread = Thread(target = handleClient, args = (client, client_name))
+        thread.start()        
+        
+        
+
+
+def set_up():
+    print('\n\t\t\t\t\t\tIP MESSENGER\n')
+    
+    global PORT
+    global IP_ADDRESS
+    global SERVER
+    
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS,PORT))
+    SERVER.listen(100)
+    
+    print('\t\t\t\SERVER IS WAITING FOR INCOMING CONNECTION...')
+    print('\n')
+    
+    acceptConnection()
+    
+
+    
+        
